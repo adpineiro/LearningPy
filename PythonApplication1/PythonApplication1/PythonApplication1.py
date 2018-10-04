a4fc2057f9528e1cfe9798516a62df7d7879c343
adpineiro <adpineiro@alumnos.uvigo.es>
2018-10-04 13:19:09 +0200

    Añadido lista y importada librería math

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,10 +1,14 @@
 #este es un fichero de prueba de python
 #primer comentario
+import math
 frase="hola mundo"
 print(frase)
 rango=range(0,10,1)
 for i in rango:
     print(i)
 j=range(2)
-list(j)
+lista=list(j)
+print(lista)
+l=math.e
+print(l)
 
