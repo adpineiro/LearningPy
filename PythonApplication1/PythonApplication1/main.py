79e5b8b5b68ab6db6c6f35d990df0494f0e58958
Adrian Piñeiro Perez <adpineiro@alumnos.uvigo.es>
2018-10-05 21:23:12 +0200

    Añadida funcion calendario

--- PythonApplication1/PythonApplication1/main.py
+++ PythonApplication1/PythonApplication1/main.py
@@ -4,8 +4,9 @@ Da la fecha y hora del sistema
 Crea matrices aleatorias
 Crea matrices a partir de datos del usuario"""
 
-
 import calculadora
+import datetime
+
 
 def calculo():
     print("Modo calculadora")
@@ -13,46 +14,62 @@ def calculo():
     seleccion = int(input())
     if seleccion == 1:
         print("Introduce los numeros a sumar")
-        num1=int(input())
-        num2=int(input())
+        num1 = int(input())
+        num2 = int(input())
         print("Resultado: ")
         print(calculadora.suma(num1, num2))
     elif seleccion == 2:
         print("Introduce los numeros a restar")
-        num1=int(input())
-        num2=int(input())
+        num1 = int(input())
+        num2 = int(input())
         print("Resultado: ")
         print(calculadora.resta(num1, num2))
     elif seleccion == 3:
         print("Introduce los numeros a multiplicar")
-        num1=int(input())
-        num2=int(input())
+        num1 = int(input())
+        num2 = int(input())
         print("Resultado: ")
         print(calculadora.multiplicacion(num1, num2))
     elif seleccion == 4:
-         print("Introduce los numeros a dividir")
-         num1 = int(input())
-         num2 = int(input())
-         print("Resultado: ")
-         print(calculadora.division(num1, num2))
-    else: print("Seleccion incorrecta")
+        print("Introduce los numeros a dividir")
+        num1 = int(input())
+        num2 = int(input())
+        print("Resultado: ")
+        print(calculadora.division(num1, num2))
+    else:
+        print("Seleccion incorrecta")
+
 
 def calendario():
     print("Modo calendario")
+    print("1:Obtener fecha\n2:Obtener hora")
+    seleccion = int(input())
+    if seleccion == 1:
+        print("Fecha")
+        print(datetime.date.today())
+    elif seleccion == 2:
+        print("Hora")
+        print(datetime.datetime.time())
+    else:
+        print("Seleccion incorrecta")
+
 
 def random_matrix():
     print("Modo matriz aleatoria")
 
+
 def user_matrix():
     print("Modo matriz del usuario")
+
 
 def exit_func():
     print("Desea salir?")
 
+
 def inicio():
     print("Inicio: Seleccione la acción\n")
     print("1: Calculadora\n2:Calendario\n3:Matriz aleatoria\n4:Matriz usuario\n5:Salir")
-    seleccion=int(input())
+    seleccion = int(input())
     if seleccion == 1:
         calculo()
     elif seleccion == 2:
@@ -67,6 +84,5 @@ def inicio():
         print("Selección incorrecta, intentelo de nuevo")
 
 
-while True :
+while True:
     inicio()
-
